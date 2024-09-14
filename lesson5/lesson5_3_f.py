660f3608732f78872e34fb515842c18b70c81cfe
opticdesigner <ku10072005@gmail.com>
2024-09-14 14:43:09 +0800

    package widget

---
+++ lesson5/lesson5_3_f.py
@@ -0,0 +1,17 @@
+import widget
+
+while True:
+    kg=0  #清除變數
+    cm=0  #清除變數
+    cm,kg = widget.input_data() # or (cm,kg) = input_data()
+
+    print(f'身高={cm},體重={kg}')
+    #BMI=calculate_bmi(kg,cm)#引述值呼叫,要按順序
+    BMI=widget.calculate_bmi(cm=cm,kg=kg)#引述名稱呼叫,可以以不依照順序
+    print(f'BMI={BMI}')
+    print(widget.get_status(BMI))
+
+    play_again = input("Do you want to continue?(y or n): ")
+    if play_again == 'n':
+        break
+print('程式結束')
