a225a5808839df6d29bddeadd42c4aae69f542a1
opticdesigner <ku10072005@gmail.com>
2024-09-21 11:43:48 +0800

    led on/off

--- pico/lesson4/main1.py
+++ pico/lesson4/main1.py
@@ -1,1 +1,23 @@
-print("Hello Python from micropython")
+# from machine import Pin
+# led = Pin("LED", Pin.OUT)
+# led.value(0)
+
+# import machine
+# led = machine.Pin("LED", machine.Pin.OUT)
+# led.value(1)
+
+from machine import Pin
+import time
+led = Pin("LED", mode=Pin.OUT)
+status = False
+while True:
+    led.on()
+    if status == False:
+        led.on()
+        status = True
+    else:
+        led.off()
+        status = False
+    time.sleep(1)
+
+    
