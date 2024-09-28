ec8b0a749b6b33dbffe9b705e1b5997ce61b9fb7
opticdesigner <ku10072005@gmail.com>
2024-09-28 10:22:24 +0800

    adc ver1

--- pico/lesson6_7/main.py
+++ pico/lesson6_7/main.py
@@ -1,1 +1,9 @@
-print("Hello Python")
+from machine import Pin
+import time
+red_led = Pin(15,Pin.OUT)
+button = Pin(14,Pin.IN,Pin.PULL_DOWN)
+
+while True:
+    if button.value(): #0:False ,有值:True
+        red_led.toggle()
+        time.sleep(0.5)
