89d5f55840ae5df557b4fd8d12de62e6394382d1
Steve Li <108560177+opticdesigner@users.noreply.github.com>
2024-09-28 22:13:53 +0800

    blynk ver1

---
+++ BlynkMQTTSamples/config.py
@@ -0,0 +1,7 @@
+# Fill in information from Blynk Device Info here.
+BLYNK_TEMPLATE_ID   = "TMPL6HLTrsKcx"
+BLYNK_TEMPLATE_NAME = "MQTT Demo"
+BLYNK_AUTH_TOKEN    = "xCt3SNgTiJiuSY6nJEycLhQq-Lon0RGb"
+
+# Change the default Blynk server. Applicable for users with a white label plan.
+BLYNK_MQTT_BROKER   = "blynk.cloud"
